ef37df72eb520f560d4e75c41843c3d988f5570e
ajah <ajahajang00@gmail.com>
2026-04-08 18:59:52 +0200

    create Routes

--- E-commerce/src/App.tsx
+++ E-commerce/src/App.tsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './output.css'
 
+import './output.css'
+import {
+
+createBrowserRouter,
+createRoutesFromElements,
+Route,RouterProvider
+}from 'react-router-dom'
+import Landing from './Landing'
+import Login from './Login'
+ const router=createBrowserRouter(createRoutesFromElements(
+ <>
+  <Route path='/' element={<Landing/>}></Route>
+ <Route path='/login' element={<Login/>}></Route>
+
+ </>))
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <RouterProvider router={router}/>
     </>
   )
 }
