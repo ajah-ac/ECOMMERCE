70352ed461ea26dacff6c94d0ab7ab35d623dce3
ajah <ajahajang00@gmail.com>
2026-04-08 18:32:52 +0200

    Chore:Create Landing page

---
+++ E-commerce/src/Landing.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const Landing = () => {
+  return (
+    <><h1>Your one stop store for all your needs</h1>
+    <Link to='/login'>Login</Link>
+    
+    </>
+  )
+}
+
+export default Landing
