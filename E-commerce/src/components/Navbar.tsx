140d634ebe85682f02ddea4a056117999ac39b45
ajah <ajahajang00@gmail.com>
2026-04-09 19:37:27 +0200

    Chore:Adjust Navbar css

--- E-commerce/src/components/Navbar.tsx
+++ E-commerce/src/components/Navbar.tsx
@@ -2,29 +2,32 @@ import React from 'react'
 import { useNavigate, NavLink } from 'react-router-dom'
 import { useAuthContext } from '../features/AuthenContext'
 const Navbar = () => {
-    type User={
-        name:string,id:number
+    type User = {
+        name: string, id: number
     }
-    const navigate=useNavigate()
-    const {logout}=useAuthContext()
-    const user=localStorage.getItem('user')
-    if(!user) return
-    const userInfor=JSON.parse(user)
-    function handleLogout(){
-logout();
-navigate ('/login', {replace:true})
+    const navigate = useNavigate()
+    const { logout } = useAuthContext()
+    const user = localStorage.getItem('user')
+    if (!user) return
+    const userInfor = JSON.parse(user)
+    function handleLogout() {
+        logout();
+        navigate('/login', { replace: true })
     }
-  return (
-    <div className='flex justify-between px-8 py-4 text-lg font-bold'>
-        <h1>Hi {userInfor.name}</h1>
-        <nav className='flex justify-evenly gap-30'>
+    return (
+        <div className='flex shadow-md mb-10 justify-between px-8 py-4 text-lg font-bold'>
+            <h1>Hi {userInfor.name}</h1>
+            <nav className='flex justify-evenly gap-30'>
 
-    <NavLink to='/categories'> Categories</NavLink>
-<button onClick={handleLogout}>Logout </button>
-                
-        </nav>
-    </div>
-  )
+                <NavLink to='/categories'> Categories</NavLink>
+                <button 
+                className='text-red-500'
+                onClick={handleLogout}
+                >Logout </button>
+
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar
