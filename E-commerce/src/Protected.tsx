c37dee9a84e004e14ab4a6807d9d9f7adb5cfa13
ajah <ajahajang00@gmail.com>
2026-04-09 19:37:49 +0200

    Chore:Add loading State

--- E-commerce/src/Protected.tsx
+++ E-commerce/src/Protected.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
 import { useAuthContext } from './features/AuthenContext'
 import {Navigate,Outlet} from 'react-router-dom'
 const Protected = () => {
-  const {user}=useAuthContext()
+  const {user,loading}=useAuthContext()
+  if(loading){
+    return<p>Loading...</p>
+  }
   if(!user)
   {
     return <Navigate to='/login' replace/>
