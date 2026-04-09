d08187e7f545c0571feca4055407462e3ad490c6
ajah <ajahajang00@gmail.com>
2026-04-09 19:42:48 +0200

    Chore:Add Categories and Categsory route

--- E-commerce/src/App.tsx
+++ E-commerce/src/App.tsx
@@ -10,18 +10,22 @@ import Landing from './Landing'
 import Login from './Login'
 import Categories from './components/Categories'
 import Protected from './Protected'
+
+import Category from './components/Category'
  const router=createBrowserRouter(createRoutesFromElements(
  <>
   <Route path='/' element={<Landing/>}></Route>
  <Route path='/login' element={<Login/>}></Route>
  <Route element={<Protected/>}>
   <Route path='/categories' element={<Categories/>}/>
+    <Route path='categories/:id' element={<Category/>}/>
+
+  
 
  </Route>
 
  </>))
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
