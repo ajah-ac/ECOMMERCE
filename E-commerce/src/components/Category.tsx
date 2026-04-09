d51560628a569ced5ce1329dea49e7a4ac4abadf
ajah <ajahajang00@gmail.com>
2026-04-09 19:41:52 +0200

    Chore:cfetch Products and Display per category

---
+++ E-commerce/src/components/Category.tsx
@@ -0,0 +1,60 @@
+import useFetchData from '../features/useFetchData'
+import { useParams } from 'react-router-dom'
+import Navbar from './Navbar'
+
+type Products={
+id:string,
+title:string;
+price:string;
+images:string[];
+category:{
+    id:number,
+    name:string
+}
+
+}
+
+function Category() {
+    const {id}=useParams()
+      const {data,loading}=useFetchData<Products[] | null>('https://api.escuelajs.co/api/v1/products')
+    
+    if(loading){
+        return <p>Loading...</p>
+    } 
+    if(!data){
+        return <> Not products found in this category</>
+    }
+    if(!id){
+        return <p>Invalid Category</p>    }
+const filter=data.filter(product=>product.category.id===+id)
+
+      return (
+        <>
+<Navbar />
+  
+
+      <h2 className="text-xl font-bold text-center my-4">
+        Products in Category {id}
+      </h2>
+
+      <div className="flex flex-wrap justify-center gap-6 p-4">
+        {filter.map((product) => (
+          <div
+            key={product.id}
+            className="w-64 bg-white shadow-md rounded-lg p-4"
+          >
+            <img
+              src={product.images[0]}
+              alt={product.title}
+              className="h-40 w-full object-cover mb-2"
+            />
+            <h3 className="font-semibold">{product.title}</h3>
+            <p className="text-blue-500">${product.price}</p>
+          </div>
+        ))}
+      </div>
+      </>
+  )
+}
+
+export default Category
