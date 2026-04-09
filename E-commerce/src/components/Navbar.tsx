7e3b4a42526358af7d9b5ce7b57a3f579a8bf87b
ajah <ajahajang00@gmail.com>
2026-04-09 18:21:34 +0200

    Chore:Change Logout Link to Button

--- E-commerce/src/components/Navbar.tsx
+++ E-commerce/src/components/Navbar.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { useNavigate, NavLink } from 'react-router-dom'
 import { useAuthContext } from '../features/AuthenContext'
 const Navbar = () => {
     type User={
         name:string,id:number
     }
+    const navigate=useNavigate()
+    const {logout}=useAuthContext()
     const user=localStorage.getItem('user')
     if(!user) return
     const userInfor=JSON.parse(user)
-    console.log()
+    function handleLogout(){
+logout();
+navigate ('/login', {replace:true})
+    }
   return (
     <div className='flex justify-between px-8 py-4 text-lg font-bold'>
         <h1>Hi {userInfor.name}</h1>
         <nav className='flex justify-evenly gap-30'>
 
     <NavLink to='/categories'> Categories</NavLink>
-    <NavLink to='/login'> Logout </NavLink>
+<button onClick={handleLogout}>Logout </button>
                 
         </nav>
     </div>
