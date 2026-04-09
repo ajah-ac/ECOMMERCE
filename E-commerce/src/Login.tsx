a027914119bb0f62ab82a232270653f04a94c479
ajah <ajahajang00@gmail.com>
2026-04-09 14:42:42 +0200

    Chore:Update login logic

--- E-commerce/src/Login.tsx
+++ E-commerce/src/Login.tsx
@@ -1,27 +1,48 @@
 import { useState } from "react"
-
-
-
+import { Navigate,useNavigate } from "react-router-dom"
+import useFetchData from "./features/useFetchData"
+import { useAuthContext } from "./features/AuthenContext"
+interface User {
+    email: string,
+    password: string
+}
 const Login = () => {
-    const [email,setEmail]=useState('')
-    const [password,setPassword]=useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate=useNavigate()
+    const {login}=useAuthContext()
+    const { data } = useFetchData<User[]>('https://api.escuelajs.co/api/v1/users')
+    function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+        e.preventDefault()
+        
+        if (!data) { return }
+        const findUser = data.find(user => user.email === email && user.password === password)
+        if (!findUser) {
+            return 
+     }
+
+        else{
+           login(findUser);
+navigate('/categories', {replace:true})}
+
+    }
 
     return (
-    <div className=" flex justify-center m-auto items-center h-screen">
-            <form action="" className="flex  rounded-xl justify-center  space-y-8 items-center flex-col w-md h-96 m-auto shadow-md align-center">
+        <div className=" flex justify-center m-auto items-center h-screen">
+            <form action="" onSubmit={handleSubmit} className="flex  rounded-xl justify-center  space-y-8 items-center flex-col w-md h-96 m-auto shadow-md align-center">
                 <input
-                onChange={e=>setEmail(e.target.value)}
-                 className="border p-2 pl-8 rounded-2xl shadow-md" type="email"  required  placeholder="Enter Email"/>
+                    onChange={e => setEmail(e.target.value)}
+                    className="border p-2 pl-8 rounded-2xl shadow-md" type="email" required placeholder="Enter Email" />
                 <input
-                 onChange={e=>setPassword(e.target.value)}
+                    onChange={e => setPassword(e.target.value)}
 
-                className="border p-2 pl-8 rounded-2xl shadow-md" type="password" 
-                placeholder="Enter password" required/>
+                    className="border p-2 pl-8 rounded-2xl shadow-md" type="password"
+                    placeholder="Enter password" required />
                 <button type='submit'
-                className="border-blue-950 p-2 pl-8 pr-8 bg-blue-200 rounded-2xl shadow-md">Login</button>
+                    className="border-blue-950 p-2 pl-8 pr-8 bg-blue-200 rounded-2xl shadow-md">Login</button>
             </form>
-</div>
-    ) 
+        </div>
+    )
 }
 
 export default Login
