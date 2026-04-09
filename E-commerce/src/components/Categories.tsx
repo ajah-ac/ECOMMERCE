75e77c8d9b251c26f7a5a97ff9cc2c34eb6b791c
ajah <ajahajang00@gmail.com>
2026-04-09 18:21:11 +0200

    Chore:Adjust Categories

--- E-commerce/src/components/Categories.tsx
+++ E-commerce/src/components/Categories.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import useFetchData from '../features/useFetchData';
+import Navbar from './Navbar';
+interface CategoType{
+  id:number,name:string;image:string
+}
 const Categories = () => {
-  return (
-    <div>Categories</div>
+  const {data}=useFetchData<CategoType[]>('https://api.escuelajs.co/api/v1/categories')
+  if(!data) {return <p>No data found</p>}
+  return (<>
+    <Navbar/>
+    <ul className='flex flex-wrap space-y-2 space-x-4 justify-center  m-auto max-w-full'>
+{data.map((category:CategoType)=><li
+className='w-64 h-64'
+ 
+key={category.id}>
+<img src={category.image} className='w-full h-full' alt="image here" />
+<h3>{category.name}</h3>
+
+</li>)}
+
+
+
+    </ul>
+    </>
   )
 }
 
