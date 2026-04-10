c8c9409f4b643855a859e10ae05f72777170917c
ajah <ajahajang00@gmail.com>
2026-04-10 15:15:34 +0200

    Chore:Design landing page UI

--- E-commerce/src/Landing.tsx
+++ E-commerce/src/Landing.tsx
@@ -1,11 +1,27 @@
-import { Link } from 'react-router-dom'
-
+import { Navigate ,useNavigate} from 'react-router-dom'
+import shoppers from './assets/shoppers.png'
+import { useAuthContext } from './features/AuthenContext'
 const Landing = () => {
+    const {user,loading}=useAuthContext();
+    if(loading) return <p> Loading ....</p>
+  const navigate=useNavigate()
+  function handlelogin(){
+navigate('/login',{replace:true})
+  }
+  if(user){
+    return <Navigate to='/categories' replace/>
+  }
   return (
-    <><h1>Your one stop store for all your needs</h1>
-    <Link to='/login'>Login</Link>
     
-    </>
+    <div className='flex gap-6 h-screen bg-blue-100 justify-evenly items-center'>
+           <img src={shoppers} alt="" />
+
+      <div>
+      <h1 className='lg:text-4xl text-2xl mb-8 xl:w-200'>Your one stop store for all your shopping needs</h1>
+         <button  onClick={handlelogin} className='hover:bg-pink-500 hover:shadow-2xs bg-red-200 px-10 py-2 rounded-2xl mt-24'>Shop now</button>
+</div>
+    
+    </div>
   )
 }
 
