294d43c190f6e31cf238bbae648239dfbb83471b
ajah <ajahajang00@gmail.com>
2026-04-10 15:16:05 +0200

    Chore:Add Home and layout routes

--- E-commerce/src/App.tsx
+++ E-commerce/src/App.tsx
@@ -11,15 +11,19 @@ import Categories from './components/Categories'
 import Protected from './Protected'
 
 import Category from './components/Category'
+import Home from './components/Home'
+import Applayout from './components/Applayout'
  const router=createBrowserRouter(createRoutesFromElements(
  <>
   <Route path='/' element={<Landing/>}></Route>
  <Route path='/login' element={<Login/>}></Route>
  <Route element={<Protected/>}>
+    <Route element={<Applayout/>}>
+   <Route path='/home' element={<Home/>}/>
   <Route path='/categories' element={<Categories/>}/>
     <Route path='categories/:id' element={<Category/>}/>
 
-  
+  </Route>
 
  </Route>
 
